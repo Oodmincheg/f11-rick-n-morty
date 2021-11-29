5192dccbeab7d0e5b31637455f08bef91aa33c76
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2021-11-29 21:56:26 +0200

    add useEffect with dependencies.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import { useEffect, useState } from 'react';
 import CharacterCard from './components/CharacterCard';
+import Select from './components/SelectGender';
 
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [gender, setGender] = useState('');
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    const requestCharacters = async () => {
+      const params = gender ? `?gender=${gender}` : '';
+
+      setLoading(true);
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/${params}`,
+      );
+      const data = await response.json();
+      setLoading(false);
+      setCharacters(data.results);
+    };
+
     requestCharacters();
-  }, []);
-
-  // fetch(url)
-  //   .then((res) => res.json())
-  //   .then((data) => setCharacters(data))
-  //   .catch((err) => console.error(err));
-
-  const requestCharacters = async () => {
-    setLoading(true);
-    const response = await fetch('https://rickandmortyapi.com/api/character');
-    const data = await response.json();
-    setLoading(false);
-    console.log('our data === > ', data);
-    setCharacters(data.results);
-  };
+  }, [gender]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -29,12 +29,25 @@ function App() {
 
   return (
     <>
-      <div>Hello rick & morty</div>
-      {characters.map(({ name, image, species, id }) => {
-        return (
-          <CharacterCard key={id} name={name} image={image} specie={species} />
-        );
-      })}
+      {loading ? (
+        <div>...Loading</div>
+      ) : (
+        <>
+          <div>Hello rick & morty</div>
+          <Select value={gender} onChange={(value) => setGender(value)} />
+          {characters.map(({ name, image, species, id, gender }) => {
+            return (
+              <CharacterCard
+                key={id}
+                name={name}
+                image={image}
+                specie={species}
+                gender={gender}
+              />
+            );
+          })}
+        </>
+      )}
     </>
   );
 }
--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -1,9 +1,10 @@
-const CharacterCard = ({ name, image, specie }) => {
+const CharacterCard = ({ name, image, specie, gender }) => {
   return (
     <div>
       <h3>{name}</h3>
       <img src={image} alt="rick and morty character" />
       <div>Specie: {specie}</div>
+      <div>Gender: {gender}</div>
     </div>
   );
 };
